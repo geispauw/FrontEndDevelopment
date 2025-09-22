d55bc598c8254e1cdb2ebee55aa95f103779ef3e
Geis <82882147+geispauw@users.noreply.github.com>
2025-09-22 17:35:02 +0200

    first page done next to details.

--- script/javascript.js
+++ script/javascript.js
@@ -2,20 +2,39 @@ const texts = [
   "Gratis bezorging voor leden",
   "Gratis retourneren binnen 30 dagen",
 ];
-let index = 0;
-const p = document.getElementById("switch-text"); // Select your target <p>
-const pSection = document.getElementById("psection")
 
-psection.style.transition = "opacity 0.5s";
-psection.style.opacity = "1";
 
-function switchText() {
-  psection.style.opacity = "0";
-  setTimeout(() => {
-    index = (index + 1) % texts.length;
-    p.textContent = texts[index];
-    psection.style.opacity = "1";
-  }, 500); // Match transition duration
-}
 
-setInterval(switchText, 5000);
+const navButton1 = document.querySelector('footer nav:nth-of-type(1) > button')
+const navButton2 = document.querySelector('footer nav:nth-of-type(2) > button')
+const navButton3 = document.querySelector('footer nav:nth-of-type(3) > button')
+const navButton4 = document.querySelector('footer nav:nth-of-type(4) > button')
+
+
+
+
+navButton1.addEventListener('click', () => {
+  const menu = navButton1.nextElementSibling; 
+  menu.classList.toggle('show');
+}) 
+
+navButton2.addEventListener('click', () => {
+  const menu = navButton2.nextElementSibling; 
+  menu.classList.toggle('show');
+}) 
+
+navButton3.addEventListener('click', () => {
+  const menu = navButton3.nextElementSibling; 
+  menu.classList.toggle('show');
+}) 
+
+navButton4.addEventListener('click', () => {
+  const menu = navButton4.nextElementSibling; 
+  menu.classList.toggle('show');
+}) 
+
+
+
+
+
+
