425e13f3c31336ef6cc3a3bfee0e8a3a1d5734c9
Geis <82882147+geispauw@users.noreply.github.com>
2025-10-01 13:18:53 +0200

    Add files via upload

---
+++ script/artikelpagina.js
@@ -0,0 +1,56 @@
+const shoeChange1 = document.querySelector(
+  "main section:nth-of-type(7) ul > li:nth-of-type(2) ul li:nth-of-type(1) button"
+);
+const shoeChange2 = document.querySelector(
+  "main section:nth-of-type(7) ul > li:nth-of-type(2) ul li:nth-of-type(2) button"
+);
+const shoeImage = document.querySelector(
+  "main section:nth-of-type(7) ul > li:nth-of-type(2) a img:first-of-type"
+);
+
+shoeChange1.addEventListener("click", (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  shoeImage.src = "../images/2_XT-6_SHADOW.jpg";
+});
+
+shoeChange2.addEventListener("click", (event) => {
+  // event.stoppropagation & preventdefault is van chatGPT: "My page is jumping back to start on button press, how do i fix this?"
+  event.stopPropagation();
+  event.preventDefault();
+  shoeImage.src = "../images/XT_SHADOW_SMALL_ALTCOLOR.jpg";
+});
+
+shoeImage.addEventListener("mouseover", () => {
+  // .includes is van chatGPT. heeft hij met de prompt hierboven ook gelijk gefixt, weet niet waarom.
+  if (shoeImage.src.includes("/images/2_XT-6_SHADOW.jpg")) {
+    shoeImage.src = "../images/2_XT-6_SHADOW_ALT.jpg";
+  } else {
+    return;
+  }
+});
+
+shoeImage.addEventListener("mouseout", () => {
+  if (shoeImage.src.includes("/images/2_XT-6_SHADOW_ALT.jpg")) {
+    shoeImage.src = "../images/2_XT-6_SHADOW.jpg";
+  } else {
+    return;
+  }
+});
+
+const checkBox = document.querySelector("section:first-of-type input");
+const hiddenItems = document.querySelectorAll(
+  "section:first-of-type ul li:nth-of-type(n+7)"
+);
+const label = document.querySelector("section:first-of-type label");
+// https://medium.com/@ryan_forrester_/hide-and-show-elements-in-javascript-a-complete-guide-e44871f15774 //
+checkBox.addEventListener("change", () => {
+  hiddenItems.forEach((item) => {
+    if (checkBox.checked) {
+      item.style.display = "list-item";
+      label.style.display = "none";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
--- script/javascript.js
+++ script/javascript.js
@@ -3,75 +3,67 @@ const texts = [
   "Gratis retourneren binnen 30 dagen",
 ];
 
+const navButton1 = document.querySelector("footer nav:nth-of-type(1) > button");
+const navButton2 = document.querySelector("footer nav:nth-of-type(2) > button");
+const navButton3 = document.querySelector("footer nav:nth-of-type(3) > button");
+const navButton4 = document.querySelector("footer nav:nth-of-type(4) > button");
+const shoeImage = document.querySelector(
+  "main section:nth-of-type(4) ul > li:nth-of-type(2) a img:first-of-type"
+);
+const shoeChange2 = document.querySelector(
+  "main section:nth-of-type(4) ul > li:nth-of-type(2) ul li:nth-of-type(2) button"
+);
+const shoeChange1 = document.querySelector(
+  "main section:nth-of-type(4) ul > li:nth-of-type(2) ul li:nth-of-type(1) button"
+);
 
-
-const navButton1 = document.querySelector('footer nav:nth-of-type(1) > button')
-const navButton2 = document.querySelector('footer nav:nth-of-type(2) > button')
-const navButton3 = document.querySelector('footer nav:nth-of-type(3) > button')
-const navButton4 = document.querySelector('footer nav:nth-of-type(4) > button')
-const shoeImage = document.querySelector('main section:nth-of-type(4) ul > li:nth-of-type(2) a img:first-of-type')
-const shoeChange2 = document.querySelector('main section:nth-of-type(4) ul > li:nth-of-type(2) a ul li:nth-of-type(2) button')
-const shoeChange1 = document.querySelector('main section:nth-of-type(4) ul > li:nth-of-type(2) a ul li:nth-of-type(1) button')
-
-
-
-shoeChange1.addEventListener('click', (event) => {
+shoeChange1.addEventListener("click", (event) => {
   event.stopPropagation();
   event.preventDefault();
-  shoeImage.src= '/images/2_XT-6_SHADOW.jpg';
-  
-}
-)
+  shoeImage.src = "../images/2_XT-6_SHADOW.jpg";
+});
 
-shoeChange2.addEventListener('click', (event) => {
+shoeChange2.addEventListener("click", (event) => {
   // event.stoppropagation & preventdefault is van chatGPT: "My page is jumping on button press, how do i fix this?"
   event.stopPropagation();
   event.preventDefault();
-  shoeImage.src= '/images/XT_SHADOW_SMALL_ALTCOLOR.jpg';
-  
-}
-)
+  shoeImage.src = "../images/XT_SHADOW_SMALL_ALTCOLOR.jpg";
+});
 
-
-shoeImage.addEventListener('mouseover', () => {
+shoeImage.addEventListener("mouseover", () => {
   // .includes is van chatGPT. heeft hij met de prompt hierboven ook gelijk gefixt, weet niet waarom.
-  if (shoeImage.src.includes('/images/2_XT-6_SHADOW.jpg')) {
-    shoeImage.src= '/images/2_XT-6_SHADOW_ALT.jpg'
+  if (shoeImage.src.includes("/images/2_XT-6_SHADOW.jpg")) {
+    shoeImage.src = "../images/2_XT-6_SHADOW_ALT.jpg";
   } else {
     return;
   }
 });
 
-shoeImage.addEventListener('mouseout', () => {
-  if (shoeImage.src.includes( '/images/2_XT-6_SHADOW_ALT.jpg')) {
-    shoeImage.src= '../images/2_XT-6_SHADOW.jpg'
+shoeImage.addEventListener("mouseout", () => {
+  if (shoeImage.src.includes("/images/2_XT-6_SHADOW_ALT.jpg")) {
+    shoeImage.src = "../images/2_XT-6_SHADOW.jpg";
   } else {
     return;
   }
 });
 
-navButton1.addEventListener('click', () => {
-  const menu = navButton1.nextElementSibling; 
-  menu.classList.toggle('show');
-}) 
+navButton1.addEventListener("click", () => {
+  const menu = navButton1.nextElementSibling;
+  menu.classList.toggle("show");
+});
 
-navButton2.addEventListener('click', () => {
-  const menu = navButton2.nextElementSibling; 
-  menu.classList.toggle('show');
-}) 
+navButton2.addEventListener("click", () => {
+  const menu = navButton2.nextElementSibling;
+  menu.classList.toggle("show");
+});
 
-navButton3.addEventListener('click', () => {
-  const menu = navButton3.nextElementSibling; 
-  menu.classList.toggle('show');
-}) 
+navButton3.addEventListener("click", () => {
+  const menu = navButton3.nextElementSibling;
+  aaaaaaa;
+  menu.classList.toggle("show");
+});
 
-navButton4.addEventListener('click', () => {
-  const menu = navButton4.nextElementSibling; 
-  menu.classList.toggle('show');
-}) 
-
-
-
-
-
-
+navButton4.addEventListener("click", () => {
+  const menu = navButton4.nextElementSibling;
+  menu.classList.toggle("show");
+});
